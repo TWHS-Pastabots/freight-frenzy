95fd5138b672b5f49ca92686f791fe1c7818f1fc
ItsAMeMurio <90408884+ItsAMeMurio@users.noreply.github.com>
2021-09-15 16:37:28 -0500

    9/15/21 Added manual drive code for Rigatoni Changed motor naming convention Added Slow Constant

--- Team16911/src/main/java/org/firstinspires/ftc/team16911/teleop/Rigatoni.java
+++ Team16911/src/main/java/org/firstinspires/ftc/team16911/teleop/Rigatoni.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.team16911.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.hardware.RobotHardware;
@@ -27,36 +29,39 @@ public class Rigatoni extends OpMode
 
     public void loop()
     {
+        // Max slow when right trigger fully pressed
+        int slowConstant = (int)Math.max(1 - gamepad1.right_trigger, .1);
+
         // Mecanum drivecode
-        double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+        double y = gamepad1.left_stick_y; // Remember, this is reversed!
         double x = gamepad1.left_stick_x; // Counteract imperfect strafing
         double rx = gamepad1.right_stick_x;
 
-        double frontLeftPower = y + x + rx;
-        double backLeftPower = y - x + rx;
-        double frontRightPower = y - x - rx;
-        double backRightPower = y + x - rx;
+        double leftFrontPower = y + x + rx;
+        double leftRearPower = y - x + rx;
+        double rightFrontPower = y - x - rx;
+        double rightRearPower = y + x - rx;
 
-        if (Math.abs(frontLeftPower) > 1 || Math.abs(backLeftPower) > 1 ||
-                Math.abs(frontRightPower) > 1 || Math.abs(backRightPower) > 1 ) {
+        if (Math.abs(leftFrontPower) > 1 || Math.abs(leftRearPower) > 1 ||
+                Math.abs(rightFrontPower) > 1 || Math.abs(rightRearPower) > 1 ) {
             // Find the largest power
             double max;
-            max = Math.max(Math.abs(frontLeftPower), Math.abs(backLeftPower));
-            max = Math.max(Math.abs(frontRightPower), max);
-            max = Math.max(Math.abs(backRightPower), max);
+            max = Math.max(Math.abs(leftFrontPower), Math.abs(leftRearPower));
+            max = Math.max(Math.abs(rightFrontPower), max);
+            max = Math.max(Math.abs(rightRearPower), max);
 
             // Divide everything by max (it's positive so we don't need to worry
             // about signs)
-            frontLeftPower /= max;
-            backLeftPower /= max;
-            frontRightPower /= max;
-            backRightPower /= max;
+            leftFrontPower /= max;
+            leftRearPower /= max;
+            rightFrontPower /= max;
+            rightRearPower /= max;
         }
 
-        hardware.leftFront.setPower(frontLeftPower);
-        hardware.leftRear.setPower(backLeftPower);
-        hardware.rightFront.setPower(frontRightPower);
-        hardware.rightRear.setPower(backRightPower);
+        hardware.leftFront.setPower(leftFrontPower * slowConstant);
+        hardware.leftRear.setPower(leftRearPower * slowConstant);
+        hardware.rightFront.setPower(rightFrontPower * slowConstant);
+        hardware.rightRear.setPower(rightRearPower * slowConstant);
     }
 
     public void stop()

9e624572a7eb6cd0808a0b31e1031b791b69b549
ItsAMeMurio <90408884+ItsAMeMurio@users.noreply.github.com>
2021-09-19 14:38:59 -0500

    Added separate Hardware for Rigatoni

---
+++ Team16911/src/main/java/org/firstinspires/ftc/team16911/autonomous/Autonomous.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.team16911.autonomous;
+
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class Autonomous extends LinearOpMode
+{
+    // Trajectory
+    private Trajectory[] trajectories;
+
+    public void runOpMode()
+    {
+    }
+}
--- Team16911/src/main/java/org/firstinspires/ftc/team16911/hardware/RigatoniHardware.java
+++ Team16911/src/main/java/org/firstinspires/ftc/team16911/hardware/RigatoniHardware.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.team16911.hardware;
 
-import static org.firstinspires.ftc.teamcode.hardware.ComponentIds.LEFT_FRONT_MOTOR;
 import static org.firstinspires.ftc.teamcode.hardware.ComponentIds.LEFT_REAR_MOTOR;
 import static org.firstinspires.ftc.teamcode.hardware.ComponentIds.RIGHT_FRONT_MOTOR;
 import static org.firstinspires.ftc.teamcode.hardware.ComponentIds.RIGHT_REAR_MOTOR;
@@ -10,6 +9,7 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.internal.system.Assert;
+import org.firstinspires.ftc.teamcode.hardware.ComponentIds;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -28,10 +28,10 @@ public class RigatoniHardware
         Assert.assertNotNull(hardwareMap);
 
         // Primary Motors
-        leftFront = hardwareMap.get(DcMotorEx.class, LEFT_FRONT_MOTOR);
-        leftRear = hardwareMap.get(DcMotorEx.class, LEFT_REAR_MOTOR);
-        rightFront = hardwareMap.get(DcMotorEx.class, RIGHT_FRONT_MOTOR);
-        rightRear = hardwareMap.get(DcMotorEx.class, RIGHT_REAR_MOTOR);
+        leftFront = hardwareMap.get(DcMotorEx.class, RigatoniIds.LEFT_FRONT_MOTOR);
+        leftRear = hardwareMap.get(DcMotorEx.class, RigatoniIds.LEFT_Rear_MOTOR);
+        rightFront = hardwareMap.get(DcMotorEx.class, RigatoniIds.RIGHT_FRONT_MOTOR);
+        rightRear = hardwareMap.get(DcMotorEx.class, RigatoniIds.RIGHT_REAR_MOTOR);
         motors = new DcMotorEx[]{leftFront, leftRear, rightFront, rightRear};
 
         // Set Zero Power Behavior and Initialize

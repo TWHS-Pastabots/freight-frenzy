29d34e484b7e5c681552c88a46bef2180bb54ffa
SpaceRage <1337american@gmail.com>
2021-11-02 16:54:40 -0500

    Updated arm code, creds to Jeffrey.

--- Team16909/src/main/java/org/firstinspires/ftc/team16909/hardware/FettuccineIds.java
+++ Team16909/src/main/java/org/firstinspires/ftc/team16909/hardware/FettuccineIds.java
@@ -7,7 +7,16 @@ public class FettuccineIds
     public static final String LEFT_REAR_MOTOR = "leftRear";
     public static final String RIGHT_FRONT_MOTOR = "rightFront";
     public static final String RIGHT_REAR_MOTOR = "rightRear";
+
+    // Supplementary Motors
     public static final String CAROUSEL = "carousel";
+    public static final String LEFT_ARM_MOTOR = "leftArm";
+    public static final String RIGHT_ARM_MOTOR = "rightArm";
+    public static final String ARM_SERVO = "armServo";
+    public static final String GRABBER = "grabber";
+
+    public static final String ARM_ENCODER = "rightArm";
+
     public static final String LEFT_ENCODER = "leftRear";
     public static final String RIGHT_ENCODER = "rightRear";
     public static final String FRONT_ENCODER = "leftFront";

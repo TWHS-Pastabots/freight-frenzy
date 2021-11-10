31e62456dca610b0c69e42323278d40b017af52c
DV_04 <68516916+DV0405@users.noreply.github.com>
2021-11-10 11:27:43 -0600

    uhhhhh major update lmao

--- Team16912/src/main/java/org/firstinspires/ftc/team16912/teleop/Linguine.java
+++ Team16912/src/main/java/org/firstinspires/ftc/team16912/teleop/Linguine.java
@@ -7,31 +7,33 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.team16912.util.LinguineHardware;
-import org.firstinspires.ftc.teamcode.hardware.RobotHardware;
+import org.firstinspires.ftc.teamcode.util.Encoder;
 
 @TeleOp(name = "Linguine")
 public class Linguine extends LinearOpMode {
 
     // Initialize
+    LinguineHardware robot = new LinguineHardware();
+
+    // Global Variables
+    private double speedMult = .75;
 
     // Checks if running
     private boolean isActive() {
         return !isStopRequested() && opModeIsActive();
     }
 
-    // Converts encoder readings to radians
-    private double encToRad(int encVal) { return Math.abs(Math.toRadians(encVal * (145.0 / 112.0))); }
-
 
     // Runs once on start
     public void runOpMode() {
 
-        LinguineHardware robot = new LinguineHardware();
         robot.init(hardwareMap, true);
 
         waitForStart();
 
         // Init
+        robot.motorArm1.setTargetPositionTolerance(5);
+        robot.motorArm2.setTargetPositionTolerance(5);
 
         // Loop
         while (isActive()) {
@@ -41,13 +43,13 @@ public class Linguine extends LinearOpMode {
             }
             // Mecanum drivecode
             double y = -gamepad1.left_stick_y; // Remember, this is reversed!
-            double x = gamepad1.left_stick_x; // Counteract imperfect strafing
+            double x = -gamepad1.left_stick_x; // Counteract imperfect strafing
             double rx = gamepad1.right_stick_x;
 
-            double frontLeftPower = y + x + rx;
-            double backLeftPower = y - x + rx;
-            double frontRightPower = y - x - rx;
-            double backRightPower = y + x - rx;
+            double frontLeftPower = y + x -  rx;
+            double backLeftPower = y + x + rx;
+            double frontRightPower = y - x + rx;
+            double backRightPower = y - x - rx;
 
             if (Math.abs(frontLeftPower) > 1 || Math.abs(backLeftPower) > 1 ||
                     Math.abs(frontRightPower) > 1 || Math.abs(backRightPower) > 1 ) {
@@ -65,57 +67,94 @@ public class Linguine extends LinearOpMode {
                 backRightPower /= max;
             }
 
-            robot.motorLeftFront.setPower(frontLeftPower);
-            robot.motorLeftRear.setPower(backLeftPower);
-            robot.motorRightFront.setPower(frontRightPower);
-            robot.motorRightRear.setPower(backRightPower);
+            robot.motorLeftFront.setPower(speedMult * frontLeftPower);
+            robot.motorLeftRear.setPower(speedMult * backLeftPower);
+            robot.motorRightFront.setPower(speedMult * frontRightPower);
+            robot.motorRightRear.setPower(speedMult * backRightPower);
 
-            if (gamepad2.dpad_up)
-            {
-                for (DcMotorEx motor : robot.motorArms)
-                {
-                    if (encToRad(motor.getCurrentPosition()) <= Math.toRadians(90))
-                    {
-                        motor.setPower(-.8 * Math.cos(encToRad(motor.getCurrentPosition())) - .3);
-                        motor.setPower(-1);
-                    }
+            if (gamepad1.triangle) speedMult = .3;
+            else if (gamepad1.circle) speedMult = 1;
+            else if (gamepad1.square) speedMult = .75;
 
-                    else if (encToRad(motor.getCurrentPosition()) >= 0)
-                        motor.setPower(-1);
+
+            if (gamepad2.dpad_up) {
+                //decrease arm power so arm does not
+                if (robot.armEncoder.getCurrentPosition() < 3000 && robot.armEncoder.getCurrentPosition() > 1200)
+                    for (DcMotorEx motor : robot.motorArms) motor.setPower(-.4);
+                else for (DcMotorEx motor : robot.motorArms) motor.setPower(-.5);
+
+            }
+            else if (gamepad2.dpad_down) {
+                if (robot.armEncoder.getCurrentPosition() < 2340) {
+                    for (DcMotorEx motor : robot.motorArms) motor.setPower(.1);
+                    if (robot.armEncoder.getCurrentPosition() < 1200)
+                        for (DcMotorEx motor : robot.motorArms) motor.setPower(0 );
                 }
+
+                else for (DcMotorEx motor : robot.motorArms) motor.setPower(.5);
+            }
+//            if (gamepad2.dpad_up)
+//            {
+//                telemetry.addLine("UP");
+//                if (encToRad(robot.armEncoder.getCurrentPosition()) > -100 && encToRad(robot.armEncoder.getCurrentPosition()) < Math.PI / 2) {
+//                    robot.motorArm1.setPower(-.4 * Math.cos(encToRad(robot.armEncoder.getCurrentPosition()) - .6));
+//                    robot.motorArm2.setPower(-.4 * Math.cos(encToRad(robot.armEncoder.getCurrentPosition()) - .6));
+//                }
+//
+//                else if (encToRad(robot.armEncoder.getCurrentPosition()) >= Math.PI / 2) {
+//                    robot.motorArm1.setPower(-.2);
+//                    robot.motorArm2.setPower(-.2);
+//                }
+//            }
+//
+//            else if (gamepad2.dpad_down)
+//            {
+//                telemetry.addLine("DOWN");
+//                if (encToRad(robot.armEncoder.getCurrentPosition()) >= Math.PI / 2) {
+//                    robot.motorArm1.setPower(.4 * Math.cos(encToRad(robot.armEncoder.getCurrentPosition()) + .6));
+//                    robot.motorArm2.setPower(.4 * Math.cos(encToRad(robot.armEncoder.getCurrentPosition()) + .6));
+//                }
+//
+//                else if (encToRad(robot.armEncoder.getCurrentPosition()) > -100 && encToRad(robot.armEncoder.getCurrentPosition()) < Math.PI / 2) {
+//                    robot.motorArm1.setPower(.2);
+//                    robot.motorArm2.setPower(.2);
+//                }
+//            }
+
+            else {
+                robot.motorArm1.setPower(0);
+                robot.motorArm2.setPower(0);
             }
 
-            else if (gamepad2.dpad_down)
-            {
-                for (DcMotorEx motor : robot.motorArms)
-                {
-                    if (encToRad(motor.getCurrentPosition()) <= Math.toRadians(90))
-                    {
-                        motor.setPower(.2 * Math.cos(encToRad(motor.getCurrentPosition())) + .3);
-                        motor.setPower(-1);
-                    }
+            if (gamepad2.dpad_right) robot.cSpinner.setDirection(DcMotorSimple.Direction.FORWARD);
+            else if (gamepad2.dpad_left) robot.cSpinner.setDirection(DcMotorSimple.Direction.REVERSE);
+            robot.cSpinner.setVelocity(gamepad2.right_trigger * 1000);
 
-
-                    else if (encToRad(motor.getCurrentPosition()) >= 0) {
-                        motor.setPower(-1);
-                    }
-                }
+            // Claw Activation
+            if (gamepad2.square) {
+                robot.servoClawLeft.setPosition(1);
+                robot.servoClawRight.setPosition(1);
             }
 
-            if (gamepad2.left_bumper) {
-                robot.cSpinner.setVelocity(3000);
+            else if (gamepad2.circle) {
+                robot.servoClawLeft.setPosition(0);
+                robot.servoClawRight.setPosition(0);
             }
 
-            else if (gamepad2.right_bumper) {
-                robot.cSpinner.setVelocity(-3000);
-            }
-
-            else robot.cSpinner.setVelocity(0);
-
-            telemetry.addData("Arm Position: ", encToRad(robot.motorArm1.getCurrentPosition()));
+            // Telemetry
+            telemetry.addData("Arm Position: ", robot.armEncoder.getCurrentPosition());
             telemetry.addData("Motor Arm 1: ", robot.motorArm1.getDirection());
             telemetry.addData("Motor Arm 2: ", robot.motorArm2.getDirection());
             telemetry.update();
         }
+
+
     }
+
+    // Converts encoder readings to radians
+    // Rad = tick * pi / 4100
+    private double encToRad(int encVal) { return Math.abs(encVal * (Math.PI / 4100) ); }
+
+
+
 }

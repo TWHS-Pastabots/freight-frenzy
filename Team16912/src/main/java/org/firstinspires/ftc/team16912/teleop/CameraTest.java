6e836dad6aaa78d47a927d05f13cd0acf55d5598
DV_04 <68516916+DV0405@users.noreply.github.com>
2022-01-07 11:24:24 -0800

    installed easyopencv

---
+++ Team16912/src/main/java/org/firstinspires/ftc/team16912/teleop/CameraTest.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.team16912.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.openftc.easyopencv.OpenCvWebcam;
+
+public class CameraTest extends LinearOpMode {
+
+    public void runOpMode() {
+    }
+}

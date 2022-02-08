58c9d35ea3879747c91153e7f8c902bec9723c14
iSuckAtCode <cordrey.brandon2@gmail.com>
2022-02-08 16:33:49 -0600

    pick up second block

--- Team16912/src/main/java/org/firstinspires/ftc/team16912/autonomous/PoseStorage.java
+++ Team16912/src/main/java/org/firstinspires/ftc/team16912/autonomous/PoseStorage.java
@@ -13,7 +13,8 @@ public class PoseStorage {
     public static Pose2d RedLeft, RedRight, BlueLeft, BlueRight;
     public static Pose2d RedCarousel, BlueCarousel, RedHub, BlueHub, RedStorageUnit,
             BlueStorageUnit, RedWarehouse, BlueWarehouse, RedWarehouseSetup, BlueWarehouseSetup,
-            BluePickup, RedPickup;
+            BluePickup, RedPickup,
+            RedWarehouseBlocks, BlueWarehouseBlocks;
 
     public static Pose2d DetStartPose(int allI, int sideI)
     {
@@ -49,7 +50,7 @@ public class PoseStorage {
         // Component poses
         RedCarousel = new Pose2d(-55.72295700521856,-53.766810012697775,Math.toRadians(269.28168016088995));
         BlueCarousel = new Pose2d(-59.43131663678813, 56.86792328414704,Math.toRadians(172.84226364522823));
-        RedHub = new Pose2d( -14.000456450593105,-54.08996787199466,Math.toRadians(271.0642161002907));
+        RedHub = new Pose2d( -6.000456450593105,-63.08996787199466,Math.toRadians(271.0642161002907));
         BlueHub = new Pose2d( -10.638173890493668, 54.95355503292278, Math.toRadians(90));
         RedStorageUnit = new Pose2d(-60.66711740422015, -36.326774389878032, Math.toRadians(0));
         BlueStorageUnit = new Pose2d(-58.664135706521755, 37.818617399773153, Math.toRadians(5));
@@ -61,6 +62,9 @@ public class PoseStorage {
         BlueWarehouse = new Pose2d();
         BluePickup = new Pose2d();
         RedPickup = new Pose2d();
+        RedWarehouseBlocks = new Pose2d(48, -48, Math.toRadians(-45));
+        BlueWarehouseBlocks = new Pose2d(55, 60, Math.toRadians(15));
+
     }
 
 }
